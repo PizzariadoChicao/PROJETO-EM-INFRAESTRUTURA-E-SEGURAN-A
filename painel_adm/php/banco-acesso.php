b51b79c696de209d07b0a4d515196a514ff4ab82
PizzariadoChicao <73076428+PizzariadoChicao@users.noreply.github.com>
2021-05-27 22:07:52 -0300

    Delete painel_adm directory

--- painel_adm/php/banco-acesso.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$servidor = "liberacaoteste.mysql.dbaas.com.br";
-$usuario = "liberacaoteste";
-$senha = "liberacao7212";
-$banco = "liberacaoteste";
-$conecta = mysqli_connect($servidor, $usuario, $senha, $banco);
-?>
--- painel_adm/php/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-#Pagina de segurança, que remove usuários que entrem indevidamente pelo link, e os envia para o painel
-header("Location: ../painel.php");
-?>
